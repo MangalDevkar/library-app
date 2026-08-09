e43b858950502f8b95dddbf75e67c0f08e744c6a
Mangal Devkar <mangaldevkar17@gmail.com>
2026-08-09 15:36:25 +0530

    Add Redux store and book slice for state management

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,26 @@
+ /**
+ * Redux Store Configuration
+ * Central state management using Redux Toolkit
+ * Manages global books state across the application
+ */
+
 import { configureStore } from '@reduxjs/toolkit';
+import bookReducer from './bookSlice';
+
+// Configure Redux store with book reducer
+export const store = configureStore({
+  reducer: {
+    books: bookReducer,
+  },
+});
+
+export default store; 
+  
+  
+  
+  
+  
+  import { configureStore } from '@reduxjs/toolkit';
 import bookReducer from './bookSlice';
 
 // Configure Redux store with book reducer
